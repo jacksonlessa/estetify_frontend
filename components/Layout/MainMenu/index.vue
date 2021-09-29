fa950a3fdded49507d7f80d456fd210df85dec8d
Jackson Lessa <jacksonj.lessa@gmail.com>
2021-09-29 13:09:03 -0300

    menu active

--- components/Layout/MainMenu/index.vue
+++ components/Layout/MainMenu/index.vue
@@ -1,35 +1,35 @@
 <template>
-  <div class="">
+  <div class="menu">
     <ul>
       <li class="mb-3">
-        <NuxtLink to="/">
+        <NuxtLink to="/" class="exact-match">
           <span class="icon is-small">
             <fa :icon="['fas', 'tachometer-alt']" />
           </span> <span class="menu-text">Dashboard</span>
         </NuxtLink>
       </li>
       <li class="mb-3">
-        <NuxtLink to="/servicos">
+        <NuxtLink to="/servicos" class="simple-match">
           <span class="icon is-small"><fa :icon="['fas', 'box']" /></span> <span class="menu-text">Serviços</span>
         </NuxtLink>
       </li>
       <li class="mb-3">
-        <NuxtLink to="/profissionais">
+        <NuxtLink to="/profissionais" class="simple-match">
           <span class="icon is-small"><fa :icon="['far', 'id-card']" /></span> <span class="menu-text">Profissionais</span>
         </NuxtLink>
       </li>
       <li class="mb-3">
-        <NuxtLink to="/clientes">
+        <NuxtLink to="/clientes" class="simple-match">
           <span class="icon is-small"><fa :icon="['far', 'address-book']" /></span> <span class="menu-text">Clientes</span>
         </NuxtLink>
       </li>
       <li class="mb-3">
-        <NuxtLink to="/atendimentos">
+        <NuxtLink to="/atendimentos" class="simple-match">
           <span class="icon is-small"><fa :icon="['far', 'calendar-alt']" /></span> <span class="menu-text">Atendimentos</span>
         </NuxtLink>
       </li>
       <li class="mb-3">
-        <NuxtLink to="/usuarios">
+        <NuxtLink to="/usuarios" class="simple-match">
           <span class="icon is-small"><fa :icon="['fas', 'users']" /></span> <span class="menu-text">Usuários</span>
         </NuxtLink>
       </li>
@@ -38,9 +38,9 @@
     <p class="menu-label">Fale conosco</p>
     <ul>
       <li class="mb-3">
-        <a>
+       <NuxtLink to="/contato" class="simple-match">
           <span class="icon is-small"><fa :icon="['fab', 'telegram-plane']" /></span> <span class="menu-text">Contato</span>
-        </a>
+        </NuxtLink>
       </li>
     </ul>
   </div>
@@ -53,3 +53,7 @@ export default {
   components: {},
 };
 </script>
+
+<style lang="scss">
+@import "./index.scss";
+</style>
